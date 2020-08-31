193a8ecfc5685124c3dbfb546352a8b4069a25b3
Kumari Shivani Singh <kumarishivanisingh14@gmail.com>
2020-09-01 01:32:42 +0530

    User Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,11 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+
+const Login = ({ login, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         email: '',
         password: ''
@@ -13,39 +17,54 @@ const Login = () => {
 
     const onSubmit = async e => {
         e.preventDefault();
-        console.log('Success!');
+        login(email, password);
     };
 
-    return <Fragment>
-        <h1 className="large text-primary">Sign In</h1>
-        <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
-        <form className="form" onSubmit={e => onSubmit(e)}>
-            <div className="form-group">
-                <input 
-                type="email" 
-                placeholder="Email Address" 
-                name="email" 
-                value={email}
-                onChange={e => onChange(e)} 
-                required 
-                />
-            </div>
-            <div className="form-group">
-                <input
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    value={password}
-                    onChange={e => onChange(e)}  
-                    minLength="6"
-                />
-            </div>
-            <input type="submit" className="btn btn-primary" value="Login" />
-        </form>
-        <p className="my-1">
-            Don't have an account? <Link to='/register'>Sign Up</Link>
-        </p>
-    </Fragment>;
+    if (isAuthenticated) {
+        return <Redirect to="/dashboard" />
+    }
+
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        name="email"
+                        value={email}
+                        onChange={e => onChange(e)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={password}
+                        onChange={e => onChange(e)}
+                        minLength="6"
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to='/register'>Sign Up</Link>
+            </p>
+        </Fragment>
+    );
 };
 
-export default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,9 @@ import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
     USER_LOADED,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from '../actions/types';
 
 const initialState = {
@@ -22,7 +24,7 @@ export default function(state = initialState, action){
                 isAuthenticated: true,
                 loading: false,
                 user: payload
-            }
+            };
         case REGISTER_SUCCESS:
             localStorage.setItem('token', payload.token);
             return {
@@ -30,16 +32,39 @@ export default function(state = initialState, action){
                 ...payload,
                 isAuthenticated: true,
                 loading: false
-            }
+            };
+        case LOGIN_SUCCESS:
+            localStorage.setItem('token', payload.token);
+            return {
+                ...state,
+                ...payload,
+                isAuthenticated: true,
+                loading: false
+            };
         case REGISTER_FAIL:
-        case AUTH_ERROR:    
             localStorage.removeItem('token');
             return {
                 ...state,
                 token: null,
                 isAuthenticated: false,
                 loading: false
-            }
+            };
+        case AUTH_ERROR:
+            localStorage.removeItem('token');
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false
+            };   
+        case LOGIN_FAIL: 
+            localStorage.removeItem('token');
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false
+            };
         default:
             return state;
     }
