dff3a13e6577c989be75996cb8a5d75e2e612273
Kumari Shivani Singh <kumarishivanisingh14@gmail.com>
2020-09-10 21:18:21 +0530

    Profile Top & Profile About

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { getProfileById} from '../../actions/profile';
 
 
@@ -18,7 +20,12 @@ const Profile = ({ getProfileById, profile: { profile }, auth, match}) => {
             </Link>
             {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className="btn btn-dark">
                 Edit Profile
-            </Link>)}
+            </Link>
+            )}
+            <div class="profile-grid my-1">
+                <ProfileTop profile={profile}/>
+                <ProfileAbout profile={profile}/>
+            </div>
         </Fragment>}
     </Fragment>;
 };
